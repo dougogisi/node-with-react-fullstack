2b4d9c09c0aff438b76d3636ba254b6f015f4102
douglas ogisi <douglas.ogisi@gmail.com>
2019-04-12 06:27:32 +0100

    reducers, actions types

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => 
+  // const request = axios.get('/api/current_user');
+
+  // return {
+  //   type: FETCH_USER,
+  //   payload: request
+  // }
+  
+  async dispatch => {
+    const res = await axios.get('/api/current_user')
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+
